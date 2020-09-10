fa196d3a92bf4cbec61c6c6df125e274281a9d4a
Simon Hofmann <mail@simon-hofmann.org>
2020-09-11 00:10:50 +0200

    (#2) Created dedicated Board state entity

---
+++ state/Board.ts
@@ -0,0 +1,19 @@
+import {ArraySchema, Schema, type} from "@colyseus/schema";
+
+export class Board extends Schema {
+    @type(["number"])
+    values: number[];
+
+    @type("number")
+    rows: number;
+
+    @type("number")
+    cols: number;
+
+    constructor(rows: number = 20, cols: number = 10, initialValue: number = 0) {
+        super();
+        this.rows = rows;
+        this.cols = cols;
+        this.values = new ArraySchema<number>(...(new Array<number>(rows * cols).fill(initialValue)));
+    }
+}
