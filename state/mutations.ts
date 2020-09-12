626b05f35b6327a235f7cbc8464f960bc04a2f73
Simon Hofmann <mail@simon-hofmann.org>
2020-09-13 01:27:39 +0200

    (#2) Added state mutation functions

---
+++ state/mutations.ts
@@ -0,0 +1,37 @@
+import {Board} from "./Board";
+import {Tetrolyso} from "./Tetrolyso";
+import {Position} from "./Position";
+
+export const queryByRowAndColumn = (board: Board) => (row: number, col: number): number => {
+    return board.values[row * board.cols + col];
+}
+
+export const setValueAtRowAndColumn = (board: Board) => (row: number, col: number, value: number): void => {
+    board.values[row * board.cols + col] = value;
+}
+
+export const addEmptyRowToBoard = (board: Board): void => {
+    const emptyRow = new Array(board.cols).fill(0);
+    addRowToBoard(board, emptyRow);
+}
+
+const addRowToBoard = (board: Board, newRow: number[]) => {
+    board.values.unshift(...newRow);
+}
+
+export const deleteRowsFromBoard = (board: Board, rowToDelete: number, amountOfRowsToDelete: number = 1) => {
+    board.values.splice(rowToDelete * board.cols, board.cols * amountOfRowsToDelete);
+}
+
+export const freezeCurrentTetrolyso = (board: Board, tetrolyso: Tetrolyso, position: Position) => {
+    const setBoardValue = setValueAtRowAndColumn(board);
+    const tetrolysoElement = queryByRowAndColumn(tetrolyso);
+
+    for (let tetrolysoRow = 0; tetrolysoRow < tetrolyso.rows; ++tetrolysoRow) {
+        for (let tetrolysoCol = 0; tetrolysoCol < tetrolyso.cols; ++tetrolysoCol) {
+            if (tetrolysoElement(tetrolysoRow, tetrolysoCol) !== 0) {
+                setBoardValue(position.row + tetrolysoRow, position.col + tetrolysoCol, tetrolyso.color);
+            }
+        }
+    }
+}
