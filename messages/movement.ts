9a83a9a070f95d7c1edce70ff7e4f881ffdfddf3
Simon Hofmann <mail@simon-hofmann.org>
2020-09-13 01:43:44 +0200

    (#3) Movement message objects to steer blocks

---
+++ messages/movement.ts
@@ -0,0 +1,17 @@
+export interface Movement {
+    row: number;
+    col: number;
+}
+
+export const LEFT = {
+    row: 0,
+    col: -1,
+};
+export const RIGHT = {
+    row: 0,
+    col: 1,
+};
+export const DOWN = {
+    row: 1,
+    col: 0,
+};
