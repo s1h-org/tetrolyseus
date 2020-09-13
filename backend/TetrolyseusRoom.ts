b0f7c78b113592c4838c20dd957fac7fc279d53c
Simon Hofmann <mail@simon-hofmann.org>
2020-09-13 18:33:59 +0200

    (#6) Initialise game state

--- backend/TetrolyseusRoom.ts
+++ backend/TetrolyseusRoom.ts
@@ -1,10 +1,13 @@
 import {Client, Room} from "colyseus";
+import {GameState} from "../state/GameState";
 
-export class TetrolyseusRoom extends Room {
+export class TetrolyseusRoom extends Room<GameState> {
+    private DEFAULT_ROWS = 20;
+    private DEFAULT_COLS = 10;
+    private DEFAULT_LEVEL = 0;
+
     onCreate(options: any) {
-        this.onMessage("type", (client, message) => {
-            // handle "type" message
-        });
+        this.setState(new GameState(this.DEFAULT_ROWS, this.DEFAULT_COLS, this.DEFAULT_LEVEL));
     }
 
     onJoin(client: Client, options: any) {
