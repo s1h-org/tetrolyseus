84d0559ab96a653decb5a06bfd7fe85f294ed53b
Simon Hofmann <mail@simon-hofmann.org>
2020-09-13 18:36:00 +0200

    (#6) Detect completed lines

--- backend/TetrolyseusRoom.ts
+++ backend/TetrolyseusRoom.ts
@@ -1,6 +1,7 @@
 import {Client, Room} from "colyseus";
 import {GameState} from "../state/GameState";
 import {Position} from "../state/Position";
+import {isRowCompleted, isRowEmpty} from "./validation";
 
 export class TetrolyseusRoom extends Room<GameState> {
     private DEFAULT_ROWS = 20;
@@ -12,6 +13,20 @@ export class TetrolyseusRoom extends Room<GameState> {
             this.state.currentPosition.row + 1,
             this.state.currentPosition.col
         );
+    }
+
+    private detectCompletedLines() {
+        let completedLines = [];
+        for (let boardRow = this.state.board.rows - 1; boardRow >= 0; --boardRow) {
+            if (isRowEmpty(this.state.board, boardRow)) {
+                break;
+            }
+
+            if (isRowCompleted(this.state.board, boardRow)) {
+                completedLines.push(boardRow);
+            }
+        }
+        return completedLines;
     }
 
     onCreate(options: any) {
