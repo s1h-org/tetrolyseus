bab6b3017d18450e03d0ed73a8557f1040d930f9
Simon Hofmann <mail@simon-hofmann.org>
2020-09-13 18:34:59 +0200

    (#6) Drop Tetrolyso one row

--- backend/TetrolyseusRoom.ts
+++ backend/TetrolyseusRoom.ts
@@ -1,10 +1,18 @@
 import {Client, Room} from "colyseus";
 import {GameState} from "../state/GameState";
+import {Position} from "../state/Position";
 
 export class TetrolyseusRoom extends Room<GameState> {
     private DEFAULT_ROWS = 20;
     private DEFAULT_COLS = 10;
     private DEFAULT_LEVEL = 0;
+
+    private dropTetrolyso() {
+        return new Position(
+            this.state.currentPosition.row + 1,
+            this.state.currentPosition.col
+        );
+    }
 
     onCreate(options: any) {
         this.setState(new GameState(this.DEFAULT_ROWS, this.DEFAULT_COLS, this.DEFAULT_LEVEL));
